fd9fd633aaa5d6a1f6fad5a8814538a854c4e5a7
luca <LucaSchmidLS1@gmail.com>
2020-09-28 16:31:09 +0200

    navigations bar scrolling anzeigen-nicht anzeigen

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const navbarLinks = document.getElementsByClassName('nav-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.display = "flex";
+  } else {
+    document.getElementById("navbar").style.display= "none";
+  }
+  prevScrollpos = currentScrollPos;
+}
